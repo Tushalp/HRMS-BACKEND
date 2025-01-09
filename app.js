c2275536f96eeceb6eb404beb098f5492f2fdab8
Tushal Patel <146264436+Tushalp@users.noreply.github.com>
2025-01-09 09:21:14 +0530

    port change

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ require('./models/leave.model');
 // require('./models/association');
 
 
-const port =  3000;
+const port =  process.env.PORT||3000;
 dotenv.config();
 const app = express();
 app.use(express.json());
